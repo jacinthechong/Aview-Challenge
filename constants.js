096db8e73f15a0594d331fe99ec5c3bde989bb8e
Jacinthe Chong <raghavjacinthe@gmail.com>
2023-07-06 23:16:56 -0400

    heroku procfile

--- constants.js
+++ constants.js
@@ -1,3 +1,3 @@
-export const TRANSLATE_API_ENDPOINT = "http://localhost:8000";
+export const TRANSLATE_API_ENDPOINT = "https://translate.argosopentech.com/";
 
 export const JOKES_API_ENDPOINT = "https://v2.jokeapi.dev/joke";
